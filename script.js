c76cf2d1f0e2017342aaab05f9b731b0c5e88a97
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-08-10 19:29:49 +0530

    implement navigator.mediaDevices.getUserMedia to access camera, record video and download recorded video

--- script.js
+++ script.js
@@ -4,7 +4,39 @@ let captureBtn = document.querySelector(".capture-btn");
 let captureBtnCont = document.querySelector(".capture-btn-cont");
 let timer = document.querySelector(".timer");
 let timerCont = document.querySelector(".timer-cont");
+let video = document.querySelector("video");
+let mediaRecorder;
+let constraints = {
+    video: true,
+    audio: false,
+};
 
+// this wil store the video mediastream.
+let chunks = [];
+navigator.mediaDevices.getUserMedia(constraints)
+.then ((stream) => {
+    video.srcObject = stream;
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.addEventListener("start", () => {
+        console.log("rec-started");
+    });
+    mediaRecorder.addEventListener("dataavailable", (e) => {
+        chunks.push(e.data);
+        console.log("rec started");
+
+    });
+    mediaRecorder.addEventListener("stop", () => {
+        let blob = new Blob(chunks, {type: "video/mp4"});
+        console.log("rec-stopped");
+        let videoURL = URL.createObjectURL(blob);
+        console.log(videoURL);
+
+        let a = document.createElement("a");
+        a.href = videoURL;
+        a.download = "myVideo.mp4";
+        a.click();
+    });
+})
 
 captureBtnCont.addEventListener("click", () => {
     captureBtn.classList.add("scale-capture");
@@ -16,9 +48,11 @@ captureBtnCont.addEventListener("click", () => {
 let isRecording = false;
 recordBtnCont.addEventListener("click", () => {
     if (!isRecording) {
+        mediaRecorder.start();
         recordBtn.classList.add("scale-record");
         timer.style.display = "flex";
     } else {
+        mediaRecorder.stop();;
         recordBtn.classList.remove("scale-record");
         timer.style.display = "none";
     }
